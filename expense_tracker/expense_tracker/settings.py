caa90cd51d7996ec4a89a43247f8864cea961447
SaadTK <tahmid.karim.saad@gmail.com>
2025-01-19 20:17:35 +0600

    Installed PostgreSQL software, created a db, linked that db to the django-app.

--- expense_tracker/expense_tracker/settings.py
+++ expense_tracker/expense_tracker/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,10 +76,17 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+load_dotenv()
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'ENGINE': 'django.db.backends.postgresql',
+     'NAME': os.getenv('DB_NAME'),
+	 'USER': os.getenv('DB_USER'),
+	 'PASSWORD': os.getenv('DB_USER_PASSWORD'),
+	 'HOST': os.getenv('DB_HOST'),
+	 #'PORT': 5432, #but this is not needed. As we might change the port later
+
     }
 }
 
